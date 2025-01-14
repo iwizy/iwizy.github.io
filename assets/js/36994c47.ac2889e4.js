23926fbf4eceb0caf6633bb5d84e974fee5df77c
Alex Agafonov <agafonov.alexander@gmail.com>
2025-01-14 16:43:35 +0300

    Updates

--- assets/js/36994c47.ac2889e4.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkiwizards_lounge=self.webpackChunkiwizards_lounge||[]).push([[9858],{5516:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-blog","id":"default"}')}}]);
---
+++ assets/js/a7456010.24834ccf.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkiwizards_lounge=self.webpackChunkiwizards_lounge||[]).push([[235],{8552:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
